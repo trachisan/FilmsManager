c7c62efaa22135e6536b858ad759d1e4f39f4e6b
trachisan <andy192001d@gmail.com>
2026-02-25 13:42:06 +0200

    init project

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace MovieManager
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello it`s me");
+            Console.WriteLine("MyFilms");
+        }
+    }
+}
